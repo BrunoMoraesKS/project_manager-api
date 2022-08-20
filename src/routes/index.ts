ccbb4c183a2482f714f34aee9b81fe74dea2e165
Bruno Moraes <brunomoraes.ti@gmail.com>
2022-08-20 14:24:48 -0300

    chore: edit gitignore

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,8 @@ import { projectsRoutes } from './projects.routes';
 const router = Router();
 
 router.use('/projects', projectsRoutes);
+router.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
 
 export { router };
